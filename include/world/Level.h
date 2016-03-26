2cae801c9455fcb78d50bd335e5eae3d4dfddfc0
FruitClover <fruitclover@gmail.com>
2016-03-27 00:01:04 +0300

    Add level class, code coverage

---
+++ include/world/Level.h
@@ -0,0 +1,14 @@
+//
+// Created by ubu on 26.03.16.
+//
+
+#ifndef PIXELS_LEVEL_H
+#define PIXELS_LEVEL_H
+
+
+class Level {
+
+};
+
+
+#endif //PIXELS_LEVEL_H
---
+++ src/world/Level.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ubu on 26.03.16.
+//
+
+#include "Level.h"
